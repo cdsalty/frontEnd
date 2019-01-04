4d38af69571d9ef0b1a08f141acbeb0e1a8694d1
Zac Crosby <Zacros7164@gmail.com>
2019-01-04 11:10:16 -0500

    added markers to map

--- maps.js
+++ maps.js
@@ -3,6 +3,16 @@ let map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map_holder'), {
         center: {lat: 33.7490, lng: -84.3880},
-        zoom: 12
+        zoom: 15
     });
+}
+
+// https://developers.google.com/maps/documentation/javascript/markers
+
+function drawMarker(crimeLat, crimeLon, crime) {
+    let marker = new google.maps.Marker({
+        position: {lat: crimeLat, lng: crimeLon},
+        map: map,
+        title: crime
+    })
 }
--- scripts.js
+++ scripts.js
@@ -12,16 +12,19 @@ $(document).ready( function () {
         // console.log("getJSON")
         crimeData.forEach((stats)=>{
             // console.log(this)
-            let incident = stats.incident_date_and_time.split("T")
-            let crime = stats.crime_class
-            let address = stats.location
+            let incident = stats.incident_date_and_time.split("T");
+            let crime = stats.crime_class;
+            let address = stats.location;
+            let crimeLat = Number(stats.latitude);
+            let crimeLon = Number(stats.longitude);
             // console.log(stats.crime_class)
             // console.log(incident[0])
             // console.log(stats.location)
-            table.row.add([crime, incident[0], address])
+            // table.row.add([crime, incident[0], address])
             // console.log("add row")
+            drawMarker(crimeLat, crimeLon, crime)
         })
-        table.draw()
+        // table.draw()
     }) //end getJSON
     
 } );// end document.ready

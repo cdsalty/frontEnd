90e014a913b1e7b4814ed8f6ab4f6d6758349eeb
cdsalty <soltiscd@gmail.com>
2019-01-08 16:04:16 -0500

    whatEverIwant

--- chart.js
+++ chart.js
@@ -8,12 +8,12 @@ function drawChart(aggravatedCount,assaultCount,autoTheftCount,burglaryCount,rap
                 label: '# of Votes',
                 data: [aggravatedCount,assaultCount,autoTheftCount,burglaryCount,rapeCount,robberyCount,theftCount],
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
                 ],
                 borderColor: [
                     'rgba(255,99,132,1)',

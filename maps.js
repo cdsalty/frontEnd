16b0569b8293a7065e5f1751b3b86ef1d286e1ac
Zac Crosby <Zacros7164@gmail.com>
2019-01-08 08:11:35 -0500

    reordered script tags to improve reliability of results, and began searchbar functionality

--- maps.js
+++ maps.js
@@ -1,7 +1,7 @@
 
 let map;
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map_holder'), {
+    crimeMap = new google.maps.Map(document.getElementById('map_holder'), {
         center: {lat: 33.7490, lng: -84.3880},
         zoom: 15
     });
@@ -11,30 +11,46 @@ function initMap() {
 // https://developers.google.com/maps/documentation/javascript/markers
 
 function drawMarker(crimeLat, crimeLon, crime) {
-    let icon = {
-        url: `./picsForProject/${crime}.png`, // url
-        scaledSize: new google.maps.Size(50, 50), // scaled size
-        origin: new google.maps.Point(0,0), // origin
-        anchor: new google.maps.Point(0, 0) // anchor
-    };
+    // let icon = {
+    //     url: `./picsForProject/${crime}.png`, // url
+    //     scaledSize: new google.maps.Size(30, 30), // scaled size
+    //     origin: new google.maps.Point(0,0), // origin
+    //     anchor: new google.maps.Point(0, 0) // anchor
+    // };
     let marker = new google.maps.Marker({
         position: {lat: crimeLat, lng: crimeLon},
-        map: map,
-        icon: icon
+        map: crimeMap,
+        // icon: icon
         // label: crime
     })
 }
 
+function centerMap(address) {
+    let latLng = getGeocode(address)
+    console.log(latLng)
+
+}
 
 
 
-
-function getAddress(crimeLocation, latLng) {
+function getGeocode(crimeLocation) {
     let geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${crimeLocation}&key=${geocodeKey}`
+
     $.getJSON(geocodeURL, (results)=>{
         // console.log(results)
         // console.log(results.results[0].geometry.location)
-        let latLng = results.results[0].geometry.location
+        // results.results[0].geometry.location
+        let latLng = []
+        latLng.push(results.results[0].geometry.location.lat)
+        latLng.push(results.results[0].geometry.location.lng)
+        // console.log(latLng)
+        requestFinished = true
         return latLng
-    })
-  }
+        // resolve(latLng)
+    })// end getJSON
+
+  }// end function
+
+//   let addressPromise = new Promise(function(resolve, reject){
+
+//   })
